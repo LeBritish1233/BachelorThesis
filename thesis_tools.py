455a03d96201af5cc416503b85f826269c5193ec
LeBritish1233 <tcmartin@bluewin.ch>
2018-01-03 17:11:01 +0100

    adds the ability to get interparticipant error

--- thesis_tools.py
+++ thesis_tools.py
@@ -47,3 +47,44 @@ def getIntraparticipantErrors(dataset, autoencoderType, nTrials):
     print('Standard deviation of the means : '+str(np.std(meanTestingErrors)))
     print('Mean of the standard deviations : '+str(np.mean(stdTestingErrors)))
     print('Standard deviation of ther standard deviations :  '+str(np.std(stdTestingErrors)))
+
+def getIntraparticipantErrorsAid(dataset, autoencoderType): 
+    nStimuli = dt.getNumberOfStimuli(dataset)
+    nSpectators = dt.getNumberOfSpectators(dataset)
+
+    testingErrors = []
+
+    dataType = dt.getDataType(dataset)
+
+    for i in range(nSpectators):
+        [trainingData, _] = dt.getData(dataset, "1-"+str(nStimuli), str(i), 1)
+        
+        [autoencoder, _] = at.buildAndTrainAutoencoder(autoencoderType, trainingData, dataType)
+
+        for j in range(nSpectators):
+            if j == i:
+                continue
+
+            [testingData, _] = dt.getData(dataset, "1-"+str(nStimuli), str(j), 1)
+
+            predictedTestingData = at.predictData(autoencoder, testingData)
+
+            testingError = dt.getAverageError(testingData, predictedTestingData)
+
+            testingErrors += [testingError]
+
+    return [np.mean(testingErrors), np.std(testingErrors)]
+
+def getInterparticipantErrors(dataset, autoencoderType, nTrials):
+    meanTestingErrors = np.zeros(nTrials)
+    stdTestingErrors = np.zeros(nTrials)
+    
+    for i in range(nTrials):
+        [meanTestingErrors[i], stdTestingErrors[i]] = getInterparticipantErrorsAid(dataset, autoencoderType)
+
+    print('\nOver '+str(nTrials)+' trials, the results for dataset '+dataset+' are as follows :')
+    print('Testing Errors :')
+    print('Mean of the means : '+str(np.mean(meanTestingErrors)))
+    print('Standard deviation of the means : '+str(np.std(meanTestingErrors)))
+    print('Mean of the standard deviations : '+str(np.mean(stdTestingErrors)))
+    print('Standard deviation of ther standard deviations :  '+str(np.std(stdTestingErrors)))
